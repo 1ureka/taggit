4a6b43cd6bf57952210012314055d7a57cf7cfe2
1ureka <summerheadquarters001@gmail.com>
2026-03-03 18:16:39 +0800

    feat: 實作標籤審查與提交功能，新增圖片預覽與標籤編輯介面

--- src/routes/tagger/+page.server.ts
+++ src/routes/tagger/+page.server.ts
@@ -1,5 +1,12 @@
 import type { PageServerLoad } from "./$types.js";
+import { getDB } from "$lib/server/db.js";
+import { getAllTags } from "$lib/server/db-query.js";
+import { getStagedFiles } from "$lib/server/helpers.js";
 
 export const load: PageServerLoad = () => {
-  return {};
+  const db = getDB();
+  return {
+    stagedFiles: getStagedFiles(),
+    allTags: getAllTags(db),
+  };
 };
