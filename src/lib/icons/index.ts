affb44addc39291f642dd49d2fc00428ec3e809a
1ureka <summerheadquarters001@gmail.com>
2026-07-17 15:05:15 +0800

    新增 IconTagPlus

--- src/lib/icons/index.ts
+++ src/lib/icons/index.ts
@@ -42,6 +42,7 @@ export { default as IconSettings } from "./IconSettings.svelte";
 export { default as IconStar } from "./IconStar.svelte";
 export { default as IconStarFilled } from "./IconStarFilled.svelte";
 export { default as IconTagFilled } from "./IconTagFilled.svelte";
+export { default as IconTagPlus } from "./IconTagPlus.svelte";
 export { default as IconUpload } from "./IconUpload.svelte";
 export { default as IconUserFilled } from "./IconUserFilled.svelte";
 export { default as IconX } from "./IconX.svelte";
